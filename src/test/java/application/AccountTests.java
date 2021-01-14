90c5046e68f5b4f89e5736b238728c3560c61bbf
Konrad-Abramowski <abramowski.konrad@gmail.com>
2021-01-14 02:26:39 +0100

    Fix testSendMoney

--- src/test/java/application/AccountTests.java
+++ src/test/java/application/AccountTests.java
@@ -11,21 +11,32 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @Category({TestEntity.class})
 @RunWith(Parameterized.class)
 public class AccountTests {
     private Dane dane = new Dane();
 
-    @Parameterized.Parameter(value = 50)
-    public float value;
+    @Parameterized.Parameter(value = 0)
+    public float transactionValue;
+
+    @Parameterized.Parameter(value = 1)
+    public float finalBalance;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data(){
+        Object[][] data1 = new Object[][]{
+                {50, 350},{100,300}};
+        return Arrays.asList(data1);
+    }
 
     @Test
     public void testSendMoney(){
         System.out.println("testSendMoney");
         Account account = dane.getAccount();
-        account.sendMoney(value);
-        assertTrue(account.getValue()==(350.0f));
+        account.sendMoney(transactionValue);
+        assertTrue(account.getValue()==(finalBalance));
     }
 
     // Testowanie setterow/getterow jest chyba bez sensu
